a732273019e61d73789873a3e5f479013cd17a3c
tomas2305 <tomas23058@gmail.com>
2023-07-20 16:29:16 -0300

    storage guardar y borrar

--- src/components/Tareas.js
+++ src/components/Tareas.js
@@ -1,19 +1,33 @@
 import { Box } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import FormTarea from './FormTarea';
 import Tarea from './Tarea';
-import { ConstructionOutlined } from '@mui/icons-material';
  
 export default function Tareas(){
 
-    const [tareas, setTareas] = useState([])
+    const [tareas, setTareas] = useState(initialTareas)
+
+    function initialTareas(){
+        console.log('Llamada initial')
+        let initialTareas = []
+        for (let i = 0; i < window.localStorage.length; i++) {
+            const key = window.localStorage.key(i);
+            const value = window.localStorage.getItem(key)
+            initialTareas.push({id:key, input:value})
+        }
+        return initialTareas;
+    }
 
     function addTarea(tarea){
         setTareas([...tareas, tarea]);
+        window.localStorage.setItem(tarea.id, tarea.input)
+        console.log('agregada')
     }
 
     function deleteTarea(id){
         setTareas(tareas.filter(tarea => tarea.id !== id));
+        console.log('borrar')
+        window.localStorage.removeItem(id);
     }
 
     return(
